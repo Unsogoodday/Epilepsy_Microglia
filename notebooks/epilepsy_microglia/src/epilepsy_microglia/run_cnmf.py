672f533b1fac42a594a5afe618d75c3cca543faa
Unsogoodday <242shizume@gmail.com>
2026-09-13 13:58:41 +0900

    run scvi, cnmf

---
+++ notebooks/epilepsy_microglia/src/epilepsy_microglia/run_cnmf.py
@@ -0,0 +1,15 @@
+import sys
+from cnmf import cNMF
+
+worker_i = int(sys.argv[1])
+total_workers = int(sys.argv[2])
+
+cnmf_obj = cNMF(
+    output_dir="/data/SJLEE/Epilepsy_Microglia/notebooks/epilepsy_microglia/data/cnmf_results/snRNA_microglia_run2_filtered",
+    name="snRNA_microglia"
+)
+
+cnmf_obj.factorize(
+    worker_i=worker_i,
+    total_workers=total_workers
+)
