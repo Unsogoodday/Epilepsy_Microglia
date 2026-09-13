f703fe0b2c1c223e21a7c5ab42064b046e6e2502
Unsogoodday <242shizume@gmail.com>
2026-09-13 23:55:13 +0900

    run non-myeloid cell qc

--- notebooks/epilepsy_microglia/src/epilepsy_microglia/run_cnmf.py
+++ notebooks/epilepsy_microglia/src/epilepsy_microglia/run_cnmf.py
@@ -5,7 +5,7 @@ worker_i = int(sys.argv[1])
 total_workers = int(sys.argv[2])
 
 cnmf_obj = cNMF(
-    output_dir="/data/SJLEE/Epilepsy_Microglia/notebooks/epilepsy_microglia/data/cnmf_results/snRNA_microglia_run2_filtered",
+    output_dir="/data/SJLEE/Epilepsy_Microglia/notebooks/epilepsy_microglia/data/cnmf_results/snRNA_microglia_run3_filtered",
     name="snRNA_microglia"
 )
 
