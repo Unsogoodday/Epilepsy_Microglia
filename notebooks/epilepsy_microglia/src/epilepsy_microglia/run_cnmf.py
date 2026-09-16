3dfc06c27f9ff324ea7998dc4250b37faa23dcb4
Unsogoodday <242shizume@gmail.com>
2026-09-16 13:36:41 +0900

    fig 4.

--- notebooks/epilepsy_microglia/src/epilepsy_microglia/run_cnmf.py
+++ notebooks/epilepsy_microglia/src/epilepsy_microglia/run_cnmf.py
@@ -5,11 +5,18 @@ worker_i = int(sys.argv[1])
 total_workers = int(sys.argv[2])
 
 cnmf_obj = cNMF(
-    output_dir="/data/SJLEE/Epilepsy_Microglia/notebooks/epilepsy_microglia/data/cnmf_results/snRNA_microglia_run3_filtered",
-    name="snRNA_microglia"
+    output_dir="/data/SJLEE/Epilepsy_Microglia/notebooks/epilepsy_microglia/data/cnmf_results/scRNA_microglia_run_2_filtered",
+    name="scRNA_microglia"
 )
 
 cnmf_obj.factorize(
     worker_i=worker_i,
     total_workers=total_workers
 )
+
+"""
+for i in {0..63}; do
+    python run_cnmf.py $i 30 &
+done
+wait
+"""
